6154668bb6b5faf42eb87bfd4f5d7ffab1efe8da
chasemlim <chasemlim@gmail.com>
2018-12-03 15:32:23 -0800

    Update todos reducer with case statements

--- todo/frontend/reducers/todos_reducer.js
+++ todo/frontend/reducers/todos_reducer.js
@@ -1,11 +1,32 @@
 import { receiveTodos, receiveTodo, RECEIVE_TODO, RECEIVE_TODOS } from '../actions/todo_actions';
+import { merge } from 'lodash/merge';
 
-export default (state = {}, action) => {
+const initialState = {
+    1: {
+        id: 1,
+        title: 'wash car',
+        body: 'with soap',
+        done: false
+    },
+    2: {
+        id: 2,
+        title: 'wash dog',
+        body: 'with shampoo',
+        done: true
+    },
+};
+
+export default (state = initialState, action) => {
+    Object.freeze(state);
     switch (action.type) {
-        // case RECEIVE_TODOS:
-        //     return [...state, action.todos];
-        // case RECEIVE_TODO:
-        //     return [...state, action.todo];
+        case RECEIVE_TODOS:
+            let todohsh = {};
+            action.todos.map (todo => todohsh[todo.id] = todo );
+            return todohsh;
+        case RECEIVE_TODO:
+            let obj = {};
+            obj[action.todo.id] = action.todo;
+            return Object.assign({}, state, obj);
         default:
             return state;
     };
--- todo/frontend/todo_redux.jsx
+++ todo/frontend/todo_redux.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import configureStore from './store/store';
+import { receiveTodos, receiveTodo } from "./actions/todo_actions";
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
+    const store = configureStore();
+    window.store = store;
+    window.receiveTodos = receiveTodos;
+    window.receiveTodo = receiveTodo;
     ReactDOM.render(
         <div className="content">
 
@@ -11,6 +16,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
         </div>, 
         root
-        );
+    );
 });
 
+// function Root() {
+//     return(
+//         <div className="content">
+
+//             <h1> Todos App </h1>
+
+//         </div>
+//     )
+// }
+    
