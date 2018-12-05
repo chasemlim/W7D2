b19cf72dd3fe1698d45b4cfd5bc20730b28341e9
chasemlim <chasemlim@gmail.com>
2018-12-04 17:29:56 -0800

    Update root component and import receiveTodo to todo list container

--- TodoApp/frontend/todo_redux.jsx
+++ TodoApp/frontend/todo_redux.jsx
@@ -7,7 +7,7 @@ import Root from './components/root';
 import allTodos from './reducers/selectors'
 
 document.addEventListener('DOMContentLoaded', () => {
-    const root = document.getElementById('root');
+    const root = document.getElementById('content');
     const store = configureStore();
     ReactDOM.render(
         <Root store = { store } />,
